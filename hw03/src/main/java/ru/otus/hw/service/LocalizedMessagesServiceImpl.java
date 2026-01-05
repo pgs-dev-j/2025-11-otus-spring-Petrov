83b9e6063d2e98d06af07b629e14d904aa154672
georgij <gosha_petrov20@rambler.ru>
2026-01-05 14:57:44 +0300

    fix: remove unnecessary @Primary from LocalizedMessagesServiceImpl

--- hw03/src/main/java/ru/otus/hw/service/LocalizedMessagesServiceImpl.java
+++ hw03/src/main/java/ru/otus/hw/service/LocalizedMessagesServiceImpl.java
@@ -2,7 +2,6 @@ package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.config.LocaleConfig;
 
@@ -10,7 +9,6 @@ import java.util.Locale;
 
 @RequiredArgsConstructor
 @Service
-@Primary
 public class LocalizedMessagesServiceImpl implements LocalizedMessagesService {
 
     private final LocaleConfig localeConfig;
